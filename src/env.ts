fb225cb9e5aab254ff5911a47f2bd618e472cfe7
Mateus Vale <127565212+mateusvalecelestino@users.noreply.github.com>
2025-06-04 11:42:56 +0100

    feat(env): changed string env var in .env file

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,23 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const envSchema = z.object({
-  // APP
-  PORT: z.coerce.number().int().positive().default(3000),
-  DATABASE_URL: z.string().url(),
+	// APP
+	PORT: z.coerce.number().int().positive().default(3000),
+	DATABASE_URL: z.string().url(),
 
-  // JWT
-  JWT_SECRET: z.string(),
-  JWT_EXPIRATION: z.coerce.number().default(604800), // 604800 -> 7d
+	// JWT
+	JWT_SECRET: z.string(),
+	JWT_EXPIRATION: z.coerce.number().default(604800), // 604800 -> 7d
 });
 
 const envSchemaParseResult = envSchema.safeParse(process.env);
 
 if (!envSchemaParseResult.success) {
-  console.error(
-    "Erro de validação das variáveis de ambiente:",
-    envSchemaParseResult.error.errors
-  );
-  process.exit(1);
+	console.error(
+		'Erro de validação das variáveis de ambiente:',
+		envSchemaParseResult.error.errors
+	);
+	process.exit(1);
 }
 
 export const env = envSchemaParseResult.data;
