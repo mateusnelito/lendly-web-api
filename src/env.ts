9005cc52b6c104c3de2b175d49018c4be7c480d9
Mateus Vale <127565212+mateusvalecelestino@users.noreply.github.com>
2025-06-05 01:00:18 +0100

    feat(env): make envSchema validation dynamic

--- src/env.ts
+++ src/env.ts
@@ -1,29 +1,36 @@
 import { z } from 'zod';
 
-const envSchema = z.object({
-	// ENVIRONMENT
+const baseEnvSchema = z.object({
 	NODE_ENV: z
 		.enum(['development', 'production', 'test'])
 		.default('development'),
 	PORT: z.coerce.number().int().positive().default(3000),
-
-	// DATABASE
-	DATABASE_URL_DEV: z.string().url(),
-	DATABASE_URL_PROD: z.string().url(),
-
-	// JWT
 	JWT_SECRET: z.string(),
-	JWT_EXPIRATION: z.coerce.number().default(604800), // 604800 -> 7d
+	JWT_EXPIRATION: z.coerce.number().default(604800), // 7d
 });
 
-const envSchemaParseResult = envSchema.safeParse(process.env);
+const devEnvSchema = baseEnvSchema.extend({
+	DATABASE_URL_DEV: z.string().url(),
+	DATABASE_URL_PROD: z.string().url().optional(),
+});
 
-if (!envSchemaParseResult.success) {
+const prodEnvSchema = baseEnvSchema.extend({
+	DATABASE_URL_PROD: z.string().url(),
+	DATABASE_URL_DEV: z.string().url().optional(),
+});
+
+const envMode = process.env.NODE_ENV as 'development' | 'production' | 'test';
+
+const envSchema = envMode === 'production' ? prodEnvSchema : devEnvSchema;
+
+const result = envSchema.safeParse(process.env);
+
+if (!result.success) {
 	console.error(
 		'Erro de validação das variáveis de ambiente:',
-		envSchemaParseResult.error.errors
+		result.error.errors
 	);
 	process.exit(1);
 }
 
-export const env = envSchemaParseResult.data;
+export const env = result.data;
