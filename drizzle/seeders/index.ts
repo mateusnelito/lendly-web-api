6d4de2eb630bf57d56f0de843825884f4dae6184
Mateus Vale <mateusvalecelestinod@gmail.com>
2026-01-05 19:13:10 +0100

    feat(database|seed): improve seed file code and perfomance

--- drizzle/seeders/index.ts
+++ drizzle/seeders/index.ts
@@ -1,26 +1,37 @@
-import { db, pool } from '../../src/db';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 import { users } from '../../src/db/schema/users';
+import { env } from '../../src/env';
 import { hashPassword } from '../../src/utils/bcrypt.util';
 
-async function main() {
+const seedPool = new Pool({
+	// Obs.: Seed only available on dev mode
+	connectionString: env.DATABASE_URL_DEV,
+	max: 1,
+	idleTimeoutMillis: 1000,
+});
+
+const db = drizzle(seedPool, { logger: true });
+
+async function seed() {
+	console.log('🌱 Plantando seeds...\n');
+
 	await db.delete(users);
 
 	const passwordHash = await hashPassword('Password@1234');
 
 	await db.insert(users).values([
-		{
-			name: 'Mateus Nelito',
-			email: 'mateusnelito@gmail.com',
-			passwordHash,
-		},
-		{
-			name: 'Ana Vale',
-			email: 'anavale@gmail.com',
-			passwordHash,
-		},
+		{ name: 'Mateus Nelito', email: 'mateusnelito@gmail.com', passwordHash },
+		{ name: 'Ana Vale', email: 'anavale@gmail.com', passwordHash },
 	]);
-
-	// Closing connection with db
-	await pool.end();
 }
-main();
+
+seed()
+	.catch(error => {
+		console.error('❌ Seed falhou:', error);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await seedPool.end();
+		console.log('\n✅ Seeds plantados!');
+	});
